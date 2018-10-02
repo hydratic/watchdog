640091f18e774452624b0c892f32a8527c0c6ed9
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-10-02 13:52:55 -0400

    Create assembler.rs

---
+++ src/assembler.rs
@@ -0,0 +1,19 @@
+#![no_std]
+
+extern crate cpuio;
+extern crate hashmap_core;
+extern crate ux;
+
+mod modules;
+
+pub fn lex(file: &str) {
+
+}
+
+pub fn parse(file: &str) {
+
+}
+
+pub fn compile(file: &str) {
+
+}
