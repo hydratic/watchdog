4a119e49687865ffd6ace01d5f00390730014430
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-10-04 11:32:48 -0400

    Update and rename interp.rs to src/interp.rs

---
+++ src/interp.rs
@@ -0,0 +1,34 @@
+#![no_std]
+
+extern crate cpuio;
+extern crate hashmap_core;
+extern crate ux;
+
+pub const ERR: i2 = 0;
+pub const FILE: &str = "empty";
+pub const TARGET: &str = ":c//";
+
+pub fn lex() {
+	loop {
+		let token = match char {
+			"1" => "int",
+			"2" => "int",
+			"3" => "int",
+			"4" => "int",
+			"5" => "int",
+			"6" => "int",
+			"7" => "int",
+			"8" => "int",
+			"9" => "int",
+			"0" => "int",
+		}
+		
+		if token == "int" {
+				
+		}
+		
+		if token == "char" {
+			
+		}
+	}
+}
