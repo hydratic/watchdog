348b931d0ef9eabe730bbf7caba4caf953c71761
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-10-10 12:18:01 -0400

    Update cursor.c

--- src/raw/vga/cursor.c
+++ src/raw/vga/cursor.c
@@ -6,3 +6,9 @@ void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
 	outb(0x3D4, 0x0B);
 	outb(0x3D5, (inb(0x3E0) & 0xE0) | cursor_end);
 }
+
+void disable_cursor()
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, 0x20);
+}
