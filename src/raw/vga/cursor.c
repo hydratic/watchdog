5f84bdaa919d5dc9b4573a09019dc9b8b8177290
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-10-10 12:18:22 -0400

    Update cursor.c

--- src/raw/vga/cursor.c
+++ src/raw/vga/cursor.c
@@ -12,3 +12,13 @@ void disable_cursor()
 	outb(0x3D4, 0x0A);
 	outb(0x3D5, 0x20);
 }
+
+void update_cursor(int x, int y)
+{
+	uint16_t pos = y * VGA_WIDTH + x;
+ 
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (uint8_t) (pos & 0xFF));
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+}
