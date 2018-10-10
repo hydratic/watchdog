bd1ae30aa3bfb5ddf4ac46b4988ef4428e1ebd55
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-10-10 12:17:44 -0400

    Create cursor.c

---
+++ src/raw/vga/cursor.c
@@ -0,0 +1,8 @@
+void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+ 
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3E0) & 0xE0) | cursor_end);
+}
