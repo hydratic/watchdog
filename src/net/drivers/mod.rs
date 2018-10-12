112e6c246db689b3f145bff61ec2ffd430a8e527
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-10-12 16:55:37 -0400

    Create mod.rs

---
+++ src/net/drivers/mod.rs
@@ -0,0 +1,24 @@
+// mod.rs
+// provides calls into the i217 driver
+// 
+// TODO:
+// all
+
+#![no_std]
+
+extern {
+  // ** MMIOUTILS **
+  pub fn MMIOUtils::read8(p_address: u64) -> Volatile<u8>;
+  pub fn MMIOUtils::read16(p_address: u64) -> Volatile<u16>;
+  pub fn MMIOUtils::read32(p_address: u64) -> Volatile<u32>;
+  pub fn MMIOUtils::read64(p_address: u64) -> Volatile<u64>;
+  
+  pub fn MMIOUtils::write8(p_address: u64, p_value: u8);
+  pub fn MMIOUtils::write16(p_address: u64, p_value: u16);
+  pub fn MMIOUtils::write32(p_address: u64, p_value: u32);
+  pub fn MMIOUtils::write64(p_address: u64, p_value: u64);
+
+  // ** PORTS **
+  pub fn Portd::outportb(p_port: u16, p_data: u8);
+  // TODO
+}
