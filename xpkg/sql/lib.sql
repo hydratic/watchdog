32a3d909e4679e50e32e64a35c57bbbd23675f9c
hydratic <33503692+hydratic@users.noreply.github.com>
2018-10-23 16:48:54 -0400

    Update lib.sql

--- xpkg/sql/lib.sql
+++ xpkg/sql/lib.sql
@@ -1,1 +1,13 @@
+--Initilize the Database
+
 CREATE DATABASE programs;
+
+CREATE TABLE program_list (
+	ProgramName varchar(255),
+	SizeOfBinary varchar(255),
+	PathOfBinary varchar(255),
+	PassedInspection varchar(255),
+);
+
+--Begin to Add Binaries
+
