3b5ae84ff72ffdd5330749658a2cdf8b62c3f225
hydratic <33503692+hydratic@users.noreply.github.com>
2018-10-23 12:21:24 -0400

    Create add_to_binary.rs

---
+++ add_to_binary.rs
@@ -0,0 +1,15 @@
+// add_to_binary.rs
+// WHEN WRITING BINARIES FOR WATCHDOG, YOU MUST ADD THIS FUNCTION
+//
+// it should be accessible via:
+//
+// use cratename::watchdog;
+//
+// name the file lib.rs
+
+pub fn switch(to: &str) {
+	match to {
+		// TODO
+		// ADD ALL BINARIES HERE
+	}
+}
