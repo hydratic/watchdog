07f9a6aa63eddf8170740cc49655ccf98a7b2f65
hydratic <33503692+hydratic@users.noreply.github.com>
2018-10-23 10:06:49 -0400

    Create lib.rs

---
+++ src/xpkg/src/lib.rs
@@ -0,0 +1,21 @@
+#![no_std]
+
+#[macro_use] extern crate watchdog_net;
+#[macro_use] extern crate xpkg_backend;
+
+use watchdog_net::drivers;
+use watchdog_net::header;
+use xpkg_backend::check;
+
+pub fn download(pkg: &str) {
+	check!()
+	
+	match res {
+		false => {
+		
+		}
+		true => {
+			break;
+		}
+	}
+}
