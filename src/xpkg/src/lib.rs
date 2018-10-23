b4e09cc3c2a1c79e1e60ceaff38cffa68c4d2e6f
hydratic <33503692+hydratic@users.noreply.github.com>
2018-10-23 12:17:25 -0400

    Update lib.rs

--- src/xpkg/src/lib.rs
+++ src/xpkg/src/lib.rs
@@ -7,15 +7,38 @@ use watchdog_net::drivers;
 use watchdog_net::header;
 use xpkg_backend::check;
 
+// TODO: Add server IP address
+pub const IPv4: &str = " ";
+pub const IPv6: &str = " ";
+
 pub fn download(pkg: &str) {
 	check!()
 	
 	match res {
 		false => {
-		
+			panic!("Error 601: Package not found!");
 		}
 		true => {
 			break;
 		}
 	}
+	
+	// begin sending packets of info to the main server
+	if DEVICE == "i217" {
+		// TODO	
+	}
+	
+	if device == "PCNET" {
+		// TODO	
+	}
+}
+
+pub fn add_source() {
+	// to add a source, we can write it into the backup kernel
+	// TODO
+}
+
+pub fn remove_source() {
+	// to remove a source, we can remove it from the backup kernel
+	// TODO
 }
