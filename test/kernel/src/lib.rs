7135ae18cf125a7118ac2f7a6df71dab386b835d
hydratic <33503692+hydratic@users.noreply.github.com>
2018-10-24 13:48:07 -0400

    Create lib.rs

---
+++ test/kernel/src/lib.rs
@@ -0,0 +1,99 @@
+// Copyright 2016 Philipp Oppermann. See the README.md
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![feature(lang_items)]
+#![feature(const_fn, unique)]
+#![feature(alloc, collections)]
+#![feature(asm)]
+#![feature(naked_functions)]
+#![feature(core_intrinsics)]
+#![no_std]
+
+extern crate rlibc;
+extern crate volatile;
+extern crate spin;
+extern crate multiboot2;
+#[macro_use]
+extern crate bitflags;
+#[macro_use]
+extern crate x86;
+#[macro_use]
+extern crate once;
+extern crate bit_field;
+#[macro_use]
+extern crate lazy_static;
+
+extern crate hole_list_allocator;
+extern crate alloc;
+#[macro_use]
+extern crate collections;
+
+#[macro_use]
+mod vga_buffer;
+mod memory;
+
+mod interrupts;
+
+#[no_mangle]
+pub extern "C" fn rust_main(multiboot_information_address: usize) {
+    // ATTENTION: we have a very small stack and no guard page
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
+
+    let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
+    enable_nxe_bit();
+    enable_write_protect_bit();
+
+    // set up guard page and map the heap pages
+    memory::init(boot_info);
+
+    // initialize our IDT
+    interrupts::init();
+
+    // trigger a breakpoint exception
+    unsafe { int!(3) };
+
+    println!("It did not crash!");
+    loop {}
+}
+
+fn enable_nxe_bit() {
+    use x86::shared::msr::{IA32_EFER, rdmsr, wrmsr};
+
+    let nxe_bit = 1 << 11;
+    unsafe {
+        let efer = rdmsr(IA32_EFER);
+        wrmsr(IA32_EFER, efer | nxe_bit);
+    }
+}
+
+fn enable_write_protect_bit() {
+    use x86::shared::control_regs::{cr0, cr0_write, CR0_WRITE_PROTECT};
+
+    unsafe { cr0_write(cr0() | CR0_WRITE_PROTECT) };
+}
+
+#[cfg(not(test))]
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[cfg(not(test))]
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern "C" fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop {}
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {
+    loop {}
+}
