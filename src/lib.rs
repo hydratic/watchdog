cc327fac4f76ece7b007f56a9446914cf8e673d8
hydratic <33503692+hydratic@users.noreply.github.com>
2018-10-24 11:17:23 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ extern crate memadvise;
 extern crate raw-cpuid;
 extern crate spin;
 extern crate ux;
+#[macro_use] extern crate watchdog_raw;
 #[cfg(test)] extern crate watchdog_test;
 extern crate volatile;
 extern crate x86_64;
@@ -22,13 +23,13 @@ use spin::Mutex;
 
 pub mod elf;
 #[macro_use] pub mod fs;
-#[macro_use] pub mod raw;
 #[macro_use] pub mod mem;
 pub mod net;
 #[macro_use] pub mod vga;
 pub mod utils;
 pub mod interrupts;
 pub mod sshell;
+pub mod oom;
 
 static PICS: Mutex<ChainedPics> =
     Mutex::new(unsafe { ChainedPics::new(0x20, 0x28) });
@@ -37,25 +38,11 @@ static KEYBOARD: Mutex<Port<u8>> = Mutex::new(unsafe {
     Port::new(0x60)
 });
 
-pub struct ERROR {
-	NO_INTERP: i2,
-	NO_MEM_ERR: i2,
-	NO_MEM_MAP_ERR: i2,
-	PATHFINDER_DOWN_ERR: i2,
-}
-
 // TODO: VM load testing
 #[no_mangle]
-pub fn os() {	
+pub fn os() {
     x86_64::instructions::interrupts::enable();
-	
-	// INIT ERROR FINDING
-	let mut err = ERROR {
-		NO_INTERP = 0;
-		NO_MEM_ERR = 0;
-		NO_MEM_MAP_ERR = 0;
-		PATHFINDER_DOWN_ERR = 0;
-	}
+	ralloc::set_oom_handler(my_handler);
 	
 	// INIT SHELL
 	sshell();
