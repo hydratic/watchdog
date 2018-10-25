e6f8326607f4cb9c5b4c45862d0449db7283920b
hydratic <33503692+hydratic@users.noreply.github.com>
2018-10-25 12:34:31 -0400

    Create DP83902.rs

---
+++ src/net/drivers/DP83902.rs
@@ -0,0 +1,22 @@
+#![no_std]
+
+extern crate volatile;
+
+use volatile::Volatile;
+
+pub const COMMAND: Volatile<u16> = 300h;
+pub const FACESTART: Volatile<u16> = 301h;
+pub const FACESTOP: Volatile<u16> = 302h;
+pub const BOUNDARY: Volatile<u16> = 303h;
+pub const TRANSMITSTATUS: Volatile<u16> = 304h;
+pub const TRANSMITPAGE: Volatile<u16> = 304h
+pub const TRANSMITBYTECOUNT0: Volatile<u16> = 305h
+pub const NCR: Volatile<u16> = 30h;
+pub const TRANSMITBYTECOUNT1: Volatile<u16> = 306h;
+pub const INTERRUPTSTATUS: Volatile<u16> = 307h;
+pub const CURRENT: Volatile<u16> = 307h;
+pub const REMOTESTARTADDRESS0: Volatile<u16> = 308h;
+pub const CRDMA0: Volatile<u16> = 308h;
+pub const REMOTESTARTADDRESS1: Volatile<u16> = 309h;
+pub const CRDMA1L: Volatile<u16> = 309h;
+pub const REMOTEBYTECOUNT: Volatile<u16> = 300ah;
