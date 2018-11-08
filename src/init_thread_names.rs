c529eda7550e315df9df93a1b253e00d273f43bd
hydratic <33503692+hydratic@users.noreply.github.com>
2018-11-08 13:43:56 -0500

    Create init_thread_names.rs

---
+++ src/init_thread_names.rs
@@ -0,0 +1,14 @@
+#![no_std]
+
+pub struct Thread {
+  	id: i8,
+  	speed_mhz: i16,
+  	_cpu: i8,
+ 	current_task: &str,
+}
+
+impl _Thread
+
+pub fn init_thread(id: i8, used_threads: &str) -> i8 {
+	
+}
