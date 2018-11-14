6ad24c8ca790b4bad78fe984f036875773f26fc8
hydratic <33503692+hydratic@users.noreply.github.com>
2018-11-14 12:25:22 -0500

    Add PIC ifs to always_io.rs

--- src/always_io.rs
+++ src/always_io.rs
@@ -5,21 +5,37 @@
 #![no_std]
 
 extern crate cpuio;
-extern crate watchdog_raw as raw;
+#[macro_use] extern crate watchdog_fs as fs;
+#[macro_use] extern crate watchdog_raw as raw;
 extern crate ux;
 
+use fs::header;
+use fs::read;
 use raw::cpu;
 use raw::header;
 use raw::ps2_keyboard;
 
 pub fn init_io() -> char {
-	get_header();
+	get_header!();
 	
 	let mut pic: i2 = 1;
 	if pic == 1 {
+		if PIC_TYPE == "8529_PIC" {
+
+		}
+	}
 	
+	task!("io", SYS_THREAD);
 	let character == get_io!();
 	thread::thread__local_stroage(character, IO_THREAD_ID);
 	
 	if pic == 1 {
+		if PIC_TYPE == "8529_PIC" {
+
+		}
+	}
+}
+		
+pub fn keybinds(path: Path) {
+	get_fs_header!();
 }
