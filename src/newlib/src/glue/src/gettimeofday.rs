a50153a0438d3894da0d62fbe251a0617317fbc6
hydratic <33503692+hydratic@users.noreply.github.com>
2018-11-20 15:02:29 -0500

    Update gettimeofday.rs

--- src/newlib/src/glue/src/gettimeofday.rs
+++ src/newlib/src/glue/src/gettimeofday.rs
@@ -1,5 +1,26 @@
 #![no_std]
 
-pub fn gettimeofday() -> i16 {
+extern crate watchdog_raw as raw;
+extern crate watchdog_ralloc as ralloc;
 
+pub struct _TimeEval {
+
+}
+
+pub struct _Timezone {
+    zone: &str,
+    setback: i8,
+}
+
+impl __TimeEval for _TimeEval {
+    type TimeEval = _TimeEval;
+}
+
+impl __Timezone for _Timezone {
+    type Timezone = _Timezone;
+}
+
+pub fn gettimeofday(p: TimeEval, z: Timezone) -> i16 {
+    let time = read_rtc("time_only");
+    return time;
 }
