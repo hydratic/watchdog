927a5ed8d1a356e022bc227f01dc965a66f8b685
hydratic <33503692+hydratic@users.noreply.github.com>
2018-12-14 21:23:03 -0500

    Create car_cdr.rs

---
+++ src/util/car_cdr.rs
@@ -0,0 +1,15 @@
+#![no_std]
+
+extern crate watchdog_ralloc as ralloc;
+
+pub fn car_cdr(s: &str) -> (&str, &str) {
+    for i in 1..5 {
+        let r = s.get(0..i);
+        match r {
+            Some(x) => return (x, &s[i..]),
+            None => (),
+        }
+    }
+
+    (&s[0..0], s)
+}
