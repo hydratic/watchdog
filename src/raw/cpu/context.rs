a1272c9eb773044c9f051d962cb9dd649925ca1f
hydratic <33503692+hydratic@users.noreply.github.com>
2018-12-14 20:57:54 -0500

    Update context.rs

--- src/raw/cpu/context.rs
+++ src/raw/cpu/context.rs
@@ -4,43 +4,22 @@
 // Note on the Design:
 //
 // watchdog handles context switching via the programs.
-// each working watchdog binary should have a function, main.
+// each working watchdog binary should call a function, main.
 // when switching between tasks, main is the function that is called.
-//
-// each binary should also have a function called answer, reserved for use
-// by the kernel. this function is called by telephone and halts the other
-// processes. then, main is called in the binary that we wish to switch to.
 
 #![no_std]
 
 extern crate ux;
-
-// crates for compiling
+extern crate watchdog_pkg_backend as pkg;
+extern crate watchdog_ralloc as ralloc;
+extern crate watchdog_utils as utils;
 extern crate watchdog_mrustc_comp as mrustc_comp;
 extern crate watchdog_telephone as telephone;
-
-use mrustc_comp::*;
-
-// required to run properly
-mod mem;
-
-// call the outside world
-// mod pkg;
 
 mod thread;
 mod sleep;
 
-pub fn context_switch(addr: u32, switch_to: &str, lang: &str, compiled: bool) {
-	let mut err: i2 = 1;
-	
-	mem_addr_map!(2, addr);
-
-	if res == "used" {
-		err = 1;
-	} else { err = 0; }
-	
-	// now we need to jump to this binary
-	// from the binary we are in
+pub fn context_switch(switch_to: &str, lang: &str, compiled: bool) {
 	match switch_to {
 		"path" => {
             if compiled == false {
@@ -57,18 +36,8 @@ pub fn context_switch(addr: u32, switch_to: &str, lang: &str, compiled: bool) {
             } else { panic!("Compiled binaries are not supported yet!"); }
 		}
 		"sys" => {
-			if task == "net" {
-				// TODO
-			}
-            if task == "security" {
-                // TODO
-            }
-            if task == "alloc" {
-                // TODO
-            }
-            if task == "tfs" {
-                // TODO
-            }
+			// NOTE: the 'lang' variable serves as a switch_to in this function
+			watchdog_utils::context_handler::handle(lang);
 		}
 	}
 }
