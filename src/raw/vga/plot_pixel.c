828b1541d78a298c1cc7902221b2a00fe84e6bb7
hydratic <33503692+hydratic@users.noreply.github.com>
2018-12-16 10:34:22 -0500

    Create plot_pixel.c

---
+++ src/raw/vga/plot_pixel.c
@@ -0,0 +1,19 @@
+/* TODO:
+ * Rest of the resolutions
+ */
+
+/* only valid for 800x600x16M */
+static void putpixel(unsigned char* screen, int x,int y, int color) {
+    unsigned where = x*3 + y*2400;
+    screen[where] = color & 255;              // BLUE
+    screen[where + 1] = (color >> 8) & 255;   // GREEN
+    screen[where + 2] = (color >> 16) & 255;  // RED
+}
+ 
+/* only valid for 800x600x32bpp */
+static void putpixel(unsigned char* screen, int x,int y, int color) {
+    unsigned where = x*4 + y*3200;
+    screen[where] = color & 255;              // BLUE
+    screen[where + 1] = (color >> 8) & 255;   // GREEN
+    screen[where + 2] = (color >> 16) & 255;  // RED
+}
